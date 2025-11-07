e0a915efdd9c379daa61f51a3e2f3daf3aa8939c
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-11-08 02:53:11 +0300

    Add Component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 function App(){
-  return <h1>MERHABA</h1>;
+  return (
+    <>
+      <Header />
+      <ProductList />
+      <List/>
+    </>
+  );
+}
+function Header(){
+  return <h1>Header</h1>;
+}
+
+const ProductList = () => (
+  <>
+  <h2>ProductList</h2>
+  <Product/>
+  <Product/>
+  <Product/>
+  <Product/>
+  </>
+);
+function Product(){
+  return <h4>Product</h4>;
+}
+
+const List = function (){
+  return <h3>Header3</h3>;
 }
 
 createRoot(document.getElementById('root')).render(
