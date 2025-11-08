2f67bc845182f3afa5dc5f679b830785c496b1de
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-11-08 03:46:49 +0300

    array items

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-function App(){
+function App() {
   return (
     <>
       <Header />
@@ -9,53 +9,55 @@ function App(){
     </>
   );
 }
-function Header(){
+function Header() {
   return <h1>Header</h1>;
 }
 
 function ProductList() {
-  const items=[
+  const items = [
     {
-    "imageUrl":"1.jpg",
-    "title":"Iphone 15",
-    "description":"Lorem ipsum dolor sit amet consectetur.",
-    "price":"70000"
+      "imageUrl": "1.jpg",
+      "title": "Iphone 15",
+      "description": "Lorem ipsum dolor sit amet consectetur.",
+      "price": "70000"
     },
     {
-    "imageUrl":"2.jpeg",
-    "title":"Iphone 16",
-    "description":"Lorem ipsum dolor sit amet consectetur.",
-    "price":"80000"
-    },{
-    "imageUrl":"3.jpg",
-    "title":"Iphone 17",
-    "description":"Lorem ipsum dolor sit amet consectetur.",
-    "price":"90000"
-    },{
-    "imageUrl":"4.jpg",
-    "title":"Iphone 18",
-    "description":"Lorem ipsum dolor sit amet consectetur.",
-    "price":"100000"
+      "imageUrl": "2.jpeg",
+      "title": "Iphone 16",
+      "description": "Lorem ipsum dolor sit amet consectetur.",
+      "price": "80000"
+    }, {
+      "imageUrl": "3.jpg",
+      "title": "Iphone 17",
+      "description": "Lorem ipsum dolor sit amet consectetur.",
+      "price": "90000"
+    }, {
+      "imageUrl": "4.jpg",
+      "title": "Iphone 18",
+      "description": "Lorem ipsum dolor sit amet consectetur.",
+      "price": "100000"
     },
   ];
-  return(
+  return (
     <>
       <h2>ProductList</h2>
-      <Product image={items[0].imageUrl} title={items[0].title} description={items[0].description} price={items[0].price}/>
-      <Product image={items[1].imageUrl} title={items[1].title} description={items[1].description} price={items[1].price}/>
-      <Product image={items[2].imageUrl} title={items[2].title} description={items[2].description} price={items[2].price}/>
-      <Product image={items[3].imageUrl} title={items[3].title} description={items[3].description} price={items[3].price}/>
+      {
+        items.map((item, index) => (
+
+          <Product key={index} ProductObj={item} />
+        ))
+      }
     </>
   )
 };
-function Product(props){
-  console.log(props)
+function Product({ ProductObj }) {
+  console.log(ProductObj)
   return (
     <div>
-      <img src={"/img/"+ props.image} alt="" />
-      <h2>{props.title}</h2>
-      <p>{props.description}</p>
-      <span>{props.price}</span>
+      <img src={"/img/" + ProductObj.imageUrl} alt="" />
+      <h2>{ProductObj.title}</h2>
+      <p>{ProductObj.description}</p>
+      <span>{ProductObj.price}</span>
     </div>
   );
 }
