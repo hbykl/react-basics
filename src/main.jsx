35124c054799139c0f156742b84f1de9c98cae79
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-11-08 03:28:40 +0300

    Props

--- src/main.jsx
+++ src/main.jsx
@@ -13,28 +13,49 @@ function Header(){
   return <h1>Header</h1>;
 }
 
-const ProductList = () => (
-  <>
-  <h2>ProductList</h2>
-  <Product/>
-  <Product/>
-  <Product/>
-  <Product/>
-  </>
-);
-function Product(){
-  const productItem ={
+function ProductList() {
+  const items=[
+    {
     "imageUrl":"1.jpg",
+    "title":"Iphone 15",
+    "description":"Lorem ipsum dolor sit amet consectetur.",
+    "price":"70000"
+    },
+    {
+    "imageUrl":"2.jpeg",
     "title":"Iphone 16",
     "description":"Lorem ipsum dolor sit amet consectetur.",
     "price":"80000"
-  };
+    },{
+    "imageUrl":"3.jpg",
+    "title":"Iphone 17",
+    "description":"Lorem ipsum dolor sit amet consectetur.",
+    "price":"90000"
+    },{
+    "imageUrl":"4.jpg",
+    "title":"Iphone 18",
+    "description":"Lorem ipsum dolor sit amet consectetur.",
+    "price":"100000"
+    },
+  ];
+  return(
+    <>
+      <h2>ProductList</h2>
+      <Product image={items[0].imageUrl} title={items[0].title} description={items[0].description} price={items[0].price}/>
+      <Product image={items[1].imageUrl} title={items[1].title} description={items[1].description} price={items[1].price}/>
+      <Product image={items[2].imageUrl} title={items[2].title} description={items[2].description} price={items[2].price}/>
+      <Product image={items[3].imageUrl} title={items[3].title} description={items[3].description} price={items[3].price}/>
+    </>
+  )
+};
+function Product(props){
+  console.log(props)
   return (
     <div>
-      <img src={"/img/"+productItem.imageUrl} alt="" />
-      <h2>{productItem.title}</h2>
-      <p>{productItem.description}</p>
-      <span>{productItem.price}</span>
+      <img src={"/img/"+ props.image} alt="" />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <span>{props.price}</span>
     </div>
   );
 }
