5bd4893ff43f09a6ab152bcc4451d3e8cee0bc3d
Hüsna BIYIKLI <108001397+hbykl@users.noreply.github.com>
2025-11-20 18:42:09 +0300

    conditional expression

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <Header />
       <ProductList />
+      <Footer />
     </>
   );
 }
@@ -19,39 +20,62 @@ function ProductList() {
       "imageUrl": "1.jpg",
       "title": "Iphone 15",
       "description": "Lorem ipsum dolor sit amet consectetur.",
-      "price": "70000"
+      "price": "70000",
+      "isActive":true
     },
     {
       "imageUrl": "2.jpeg",
       "title": "Iphone 16",
       "description": "Lorem ipsum dolor sit amet consectetur.",
-      "price": "80000"
+      "price": "80000",
+      "isActive":false
+
     }, {
       "imageUrl": "3.jpg",
       "title": "Iphone 17",
       "description": "Lorem ipsum dolor sit amet consectetur.",
-      "price": "90000"
+      "price": "90000",
+      "isActive":true
+
     }, {
       "imageUrl": "4.jpg",
       "title": "Iphone 18",
       "description": "Lorem ipsum dolor sit amet consectetur.",
-      "price": "100000"
+      "price": "100000",
+      "isActive":true
+
     },
   ];
+  
   return (
     <>
       <h2>ProductList</h2>
-      {
-        items.map((item, index) => (
+      {items.length>0?
+      (
+        <div id="itemList">
+        {
+          items.map((item, index) => (
 
-          <Product key={index} ProductObj={item} />
-        ))
+            <Product key={index} ProductObj={item} />
+          ))
+
+        }
+      </div>
+      ):
+      (
+        <p>
+          Şu anda satışta ürün yoktur.
+        </p>
+      )
       }
+      
     </>
   )
 };
 function Product({ ProductObj }) {
   console.log(ProductObj)
+  console.log(!ProductObj.isActive, ProductObj.title)
+  if(!ProductObj.isActive)  return null;
   return (
     <div>
       <img src={"/img/" + ProductObj.imageUrl} alt="" />
@@ -60,6 +84,31 @@ function Product({ ProductObj }) {
       <span>{ProductObj.price}</span>
     </div>
   );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  console.log(hour);
+  const openHour = 10
+  const closeHour = 20
+  const isOpen = hour >= openHour && hour <= closeHour;
+  return (
+    <footer>
+      {
+        isOpen ? (
+          <p>
+            saat {closeHour}'a kadar açığız.
+          </p>
+        ) :
+          (
+            <p>
+              Şu anda kapalıyız. Saat {openHour} 'da görüşmek üzere
+            </p>
+          )
+      }
+    </footer>
+  )
+
 }
 
 // const List = function (){
